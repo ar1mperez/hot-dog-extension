691f32ee6ef84e5ee34c60da62e367e5938688e7
MrSprinklez <red4ari@gmail.com>
2018-02-07 23:33:45 -0500

    Added toggle button

--- myscript.js
+++ myscript.js
@@ -1,19 +1,77 @@
 
 
-var myNodelist = document.querySelectorAll("img");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    myNodelist[i].src = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Hot_dog_with_mustard.png/1200px-Hot_dog_with_mustard.png";
-}
+
+chrome.storage.sync.get('state', function(data) {
+    console.log("Started from the bottom");
+    if (data.state == null) {
+        var save = {};
+        save['state'] = 'on';
+
+        chrome.storage.sync.set(save, function() {
+            console.log('Settings saved');
+        });
+
+    }
+
+});
+/*
+chrome.runtime.onStartup.addListener(function () {
+    var save = {};
+    save["myKey"] = testPrefs;
+
+    chrome.storage.sync.set(save, function() {
+        console.log('Settings saved');
+    });
+});
+
+*/
+
+chrome.storage.sync.get('state', function(data) {
+    if (data.state === 'on') {
+        var myNodelist = document.querySelectorAll("img");
+        var i;
+        for (i = 0; i < myNodelist.length; i++) {
+            myNodelist[i].src = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Hot_dog_with_mustard.png/1200px-Hot_dog_with_mustard.png";
+        }
+
+    }
+});
+
 
 
 document.addEventListener('DOMNodeInserted', function(){
-    var mylist = document.querySelectorAll("img");
-    var j;
-    for (j = 0; j < mylist.length; j++) {
-        mylist[j].src = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Hot_dog_with_mustard.png/1200px-Hot_dog_with_mustard.png";
-    }
+    chrome.storage.sync.get('state', function(data) {
+        if (data.state == null) {
+            console.log("FAKKKK");
+        }
+        console.log(data.state);
+        if (data.state === 'on') {
+            var mylist = document.querySelectorAll("img");
+            var j;
+            for (j = 0; j < mylist.length; j++) {
+                mylist[j].src = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Hot_dog_with_mustard.png/1200px-Hot_dog_with_mustard.png";
+            }
+        }
+    });
 
+});
+
+//When button is pressed change status to on or off
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('toggle');
+    checkPageButton.addEventListener('click', function() {
+        chrome.storage.sync.get('state', function(data) {
+            if (data.state === 'on') {
+                chrome.storage.sync.set({state: 'off'});
+            //do something, removing the script or whatever
+            } else {
+                chrome.storage.sync.set({state: 'on'});
+                //inject your script
+            }
+            chrome.tabs.reload(function(){});
+        });
+
+    });
 });
 
 /*
--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+/*console.log("Hey there!\n");
+window.open("http://www.ariperez.me",'_blank');
 document.addEventListener('DOMContentLoaded', function() {
     var myNodelist = document.querySelectorAll("img");
     var i;
@@ -9,3 +11,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 }
+
+
+
+function changeState() {
+    alert("WHAT THE!");
+    chrome.storage.sync.get('state', function(data) {
+        if (data.state === 'on') {
+            console.log("OFF!\n");
+            chrome.storage.sync.set({state: 'off'});
+        //do something, removing the script or whatever
+        } else {
+            chrome.storage.sync.set({state: 'on'});
+            //inject your script
+      }
+    });
+}
+*/
